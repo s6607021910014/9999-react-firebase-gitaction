2ee5600ed5765c801c2b72bf44329bb7e0ac8577
ANT-LAB <s6607021910014@email.kmutnb.ac.th>
2025-05-03 15:03:11 +0700

    worlkflow added7

--- src/report.jsx
+++ src/report.jsx
@@ -62,7 +62,7 @@ export default class report extends Component {
         <Navbar bg="primary" variant="dark">
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
-            <div>Mr .Siwakorn Banluesapy  </div>
+            <div>Mr .Siwakorn Banluesapy Lab Firebase </div>
           </Container>
         </Navbar>
         <Container>
