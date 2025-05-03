4685c1953540eb346fa03bf8d357db114fd97e7e
ANT-LAB <s6607021910014@email.kmutnb.ac.th>
2025-05-03 15:28:14 +0700

    update name function

--- src/report.jsx
+++ src/report.jsx
@@ -62,7 +62,7 @@ export default class report extends Component {
         <Navbar bg="primary" variant="dark">
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
-            <div>Mr .Siwakorn Banluesapy Lab Firebase </div>
+            <div>Mr .Siwakorn Banluesapy</div>
           </Container>
         </Navbar>
         <Container>
